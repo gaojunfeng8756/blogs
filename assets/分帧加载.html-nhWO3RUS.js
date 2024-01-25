0d3bce33e1f29acbab1527517243c061250d41ee
gaojunfeng8756 <37437679+gaojunfeng8756@users.noreply.github.com>
2024-01-25 09:03:07 +0000

    Deploying to gh-pages from @ gaojunfeng8756/blogs@51f26b175fb0174d1bb041c528e87fbb35a0d4b2 🚀

---
+++ assets/分帧加载.html-nhWO3RUS.js
@@ -0,0 +1,63 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as i,f as s}from"./app--gl3tdV2.js";const l={},d=s(`<h2 id="分帧加载" tabindex="-1"><a class="header-anchor" href="#分帧加载" aria-hidden="true">#</a> 分帧加载</h2><p>我们在生成item的时候，很容易造成页面动画卡顿，这个时候，我们需要考虑去分帧加载，可以参考以下代码</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>//初始化item
+initItem(index){
+
+}
+  /**
+ * 实现分帧加载
+ */
+    async framingLoad(length: number) {
+        await this.executePreFrame(this._getItemGenerator(length), 1);
+    }
+
+    private *_getItemGenerator(length: number) {
+        for (let i = 0; i &lt; length; i++) {
+            yield this.initItem(i);
+        }
+    }
+
+    /**
+     * 分帧执行 Generator 逻辑
+     *
+     * @param generator 生成器
+     * @param duration 持续时间（ms）
+     *          每次执行 Generator 的操作时，最长可持续执行时长。
+     *          假设值为8ms，那么表示1帧（总共16ms）下，分出8ms时间给此逻辑执行
+     */
+    private executePreFrame(generator: Generator, duration: number) {
+        return new Promise((resolve, reject) =&gt; {
+            let gen = generator;
+            // 创建执行函数
+            let execute = () =&gt; {
+
+                // 执行之前，先记录开始时间戳
+                let startTime = new Date().getTime();
+
+                // 然后一直从 Generator 中获取已经拆分好的代码段出来执行
+                for (let iter = gen.next(); ; iter = gen.next()) {
+
+                    // 判断是否已经执行完所有 Generator 的小代码段
+                    // 如果是的话，那么就表示任务完成
+                    if (iter == null || iter.done) {
+                        resolve(null);
+                        return;
+                    }
+
+                    // 每执行完一段小代码段，都检查一下是否
+                    // 已经超过我们分配给本帧，这些小代码端的最大可执行时间
+                    if (new Date().getTime() - startTime &gt; duration) {
+
+                        // 如果超过了，那么本帧就不在执行，开定时器，让下一帧再执行
+                        this.scheduleOnce(() =&gt; {
+                            execute();
+                        });
+                        return;
+                    }
+                }
+            };
+
+            // 运行执行函数
+            execute();
+        });
+    }
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3),r=[d];function v(a,t){return n(),i("div",null,r)}const u=e(l,[["render",v],["__file","分帧加载.html.vue"]]);export{u as default};
