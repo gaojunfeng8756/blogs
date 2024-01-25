09cd88233b748a7a9ff8207d603109da87c933ee
嘻嘻 <1037775604@qq.com>
2024-01-25 17:52:08 +0800

    fix

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,22 +2,26 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  // "/demo/",
   {
-    text: "博文超链接",
-    icon: "pen-to-square",
-    prefix: "/posts/",
-    children: [
-      {
-        text: "分帧加载",
-        icon: "pen-to-square",
-        prefix: "cocosCreator2.x/",
-        children: [
-          { text: "分帧加载", icon: "pen-to-square", link: "分帧加载" },
-        ],
-      }
-    ],
+    text:"博客目录",
+    icon:"book",
+    link: "/posts"
   },
+  // {
+  //   text: "博文超链接",
+  //   icon: "pen-to-square",
+  //   prefix: "/posts/",
+  //   children: [
+  //     {
+  //       text: "分帧加载",
+  //       icon: "pen-to-square",
+  //       prefix: "cocosCreator2.x/",
+  //       children: [
+  //         { text: "分帧加载", icon: "pen-to-square", link: "分帧加载" },
+  //       ],
+  //     }
+  //   ],
+  // },
   // {
   //   text: "V2 文档",
   //   icon: "book",
