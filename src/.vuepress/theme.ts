39eaa19f4afe8d17f18a29f5028b77385f415d08
嘻嘻 <1037775604@qq.com>
2024-01-26 11:18:22 +0800

    fix

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -34,37 +34,6 @@ export default hopeTheme({
     intro: "/intro.html",
     medias: {
       // Baidu: "https://example.com",
-      // BiliBili: "https://example.com",
-      // Bitbucket: "https://example.com",
-      // Dingding: "https://example.com",
-      // Discord: "https://example.com",
-      // Dribbble: "https://example.com",
-      // Email: "mailto:info@example.com",
-      // Evernote: "https://example.com",
-      // Facebook: "https://example.com",
-      // Flipboard: "https://example.com",
-      // Gitee: "https://example.com",
-      // GitHub: "https://example.com",
-      // Gitlab: "https://example.com",
-      // Gmail: "mailto:info@example.com",
-      // Instagram: "https://example.com",
-      // Lark: "https://example.com",
-      // Lines: "https://example.com",
-      // Linkedin: "https://example.com",
-      // Pinterest: "https://example.com",
-      // Pocket: "https://example.com",
-      // QQ: "https://example.com",
-      // Qzone: "https://example.com",
-      // Reddit: "https://example.com",
-      // Rss: "https://example.com",
-      // Steam: "https://example.com",
-      // Twitter: "https://example.com",
-      // Wechat: "https://example.com",
-      // Weibo: "https://example.com",
-      // Whatsapp: "https://example.com",
-      // Youtube: "https://example.com",
-      // Zhihu: "https://example.com",
-      // MrHope: ["https://gaojunfeng8756.github.io/blogs/", MR_HOPE_AVATAR],
     },
   },
 
@@ -90,18 +59,43 @@ export default hopeTheme({
     //   serverURL: "https://waline-comment.vuejs.press",
     // },
 
+    // 缓存控制
+			pwa: {
+				update: "hint",
+				maxSize: 0
+			},
+
+			// 评论插件配置
+			comment: {
+			
+				// provider: "Giscus",
+				// repo: "1226085293/1226085293.github.io",
+				// repoId: "R_kgDOJ6pmwg",
+				// category: "General",
+				// categoryId: "DIC_kwDOJ6pmws4CX20J",
+				// mapping: "title",
+			},
     // all features are enabled for demo, only preserve features you need here
     mdEnhance: {
       align: true,
       attrs: true,
+      chart: true,
       codetabs: true,
-      component: true,
       demo: true,
+      echarts: true,
       figure: true,
+      flowchart: true,
+      gfm: true,
       imgLazyload: true,
       imgSize: true,
       include: true,
+      katex: true,
       mark: true,
+      mermaid: true,
+      playground: {
+        presets: ["ts", "vue"],
+      },
+      // presentation: ["highlight", "math", "search", "notes", "zoom"],
       stylize: [
         {
           matcher: "Recommended",
@@ -119,6 +113,7 @@ export default hopeTheme({
       sup: true,
       tabs: true,
       vPre: true,
+      vuePlayground: true,
 
       // install chart.js before enabling it
       // chart: true,
