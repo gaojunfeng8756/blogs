c90221377b0cae031be8788f5a2eb6a46f2f0fb0
嘻嘻 <1037775604@qq.com>
2024-01-26 11:42:24 +0800

    添加搜索

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -18,14 +18,13 @@ export default hopeTheme({
   repo: "gaojunfeng8756/blogs",
 
   docsDir: "src",
-
   // navbar
   navbar,
 
   navbarLayout: {
     start: ["Brand"],
     center: ["Links"],
-    end: [ "Outlook"],
+    end: [ "Search","Outlook"],
   },
   // sidebar
   sidebar,
@@ -55,7 +54,7 @@ export default hopeTheme({
 
   plugins: {
     blog: true,
-
+    search: true,
     // install @waline/client before enabling it
     // WARNING: This is a test server for demo only.
     // You should create and use your own comment service in production.
